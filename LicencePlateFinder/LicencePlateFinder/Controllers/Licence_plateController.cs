0d1937963ef77f2695ef90c13082e6a6a354ce65
kovacsnorb <kovacsnorb@gmail.com>
2017-11-17 09:54:57 +0100

    MVC structure is done, no database connection yet.

---
+++ LicencePlateFinder/LicencePlateFinder/Controllers/Licence_plateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LicencePlateFinder.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LicencePlateFinder.Controllers
+{
+    public class Licence_plateController : Controller
+    {
+        Licence_plateRepository licence_PlateRepository;
+
+        public Licence_plateController(Licence_plateRepository licence_PlateRepository)
+        {
+            this.licence_PlateRepository = licence_PlateRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
